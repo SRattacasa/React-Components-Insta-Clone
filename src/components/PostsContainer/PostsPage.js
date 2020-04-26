3694d2e48af1d6419ef83e99fc2271771754fd81
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2020-04-26 14:43:09 -0400

    Got the basic posts props and data working

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,14 +1,24 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import dummyData from '../../dummy-data'
 
 const PostsPage = () => {
   // set up state for your data
+  const [postState, setPoststate] = useState();
+  console.log(dummyData);
+  
   return (
     <div className="posts-container-wrapper">
+      {dummyData.map( e => { 
+        return <Post post = {e}
+
+         />  
+        })} 
+      
       {/* map through data here to return a Post and pass data as props to Post */}
     </div>
   );
